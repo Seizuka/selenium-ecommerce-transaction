078262924dee9bf4374f4f4e99256e94d9d098d9
Seizuka <adzanibillian@gmail.com>
2025-07-12 23:52:33 +0700

    add get otp from gmail using imap

--- transaction_test_cases/positive_transaction.py
+++ transaction_test_cases/positive_transaction.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from transaction_object.transaction_page import TransactionPage
 from selenium.webdriver.chrome.service import Service
+from utils.gmail_utils import get_otp_from_gmail_imap
 import time
 import json
 import config
 import undetected_chromedriver as uc
+
 
 
 def run():
@@ -15,17 +17,23 @@ def run():
     driver = uc.Chrome(service=service)
     driver.maximize_window()
 
-    driver.get("https://www.tokopedia.com/")
+    driver.get("https://www.blibli.com/")
 
     transaction_page = TransactionPage(driver)
-    transaction_page.click_close_pop_up()
     transaction_page.click_btn_login()
 
-    transaction_page.fill_login(config.PHONE_NUMBER)
-    transaction_page.click_send_otp()
-    #transaction_page.fill_login(config.PIN)
+    transaction_page.fill_login(config.EMAIL)
+    transaction_page.click_close_pop_up()
+    time.sleep(3)
 
-    transaction_page.fill_search(
-        data['search']
-    )
+    otp_code = get_otp_from_gmail_imap(config.EMAIL, config.APP_PASSWORD)
+    print("OTP Retrieved:", otp_code)
+
+    if otp_code:
+        transaction_page.fill_otp(otp_code)
+        # transaction_page.fill_search(data['search'])
+    else:
+        print("OTP not found.")
+    
+    time.sleep(10)
     driver.quit()
